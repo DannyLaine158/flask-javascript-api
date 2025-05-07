2ed04436141536457356f91a785c38f5319c04c1
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2025-05-07 15:47:50 -0600

    Add files via upload

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -4,37 +4,47 @@ let btnLogin = document.getElementById("btnLogin");
 btnLogin.addEventListener("click", () => {
     let varUsername = document.getElementById("username").value;
     let varPassword = document.getElementById("password").value;
+    const loader = document.getElementById("login-loader");
+    
+    loader.style.display = 'block'; // Muestra el loader
 
-    // Unión entre python y javascript
-    fetch('http://127.0.0.1:5000/api/login', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            username: varUsername,
-            password: varPassword
+    setTimeout(() => {
+        // Unión entre python y javascript
+        fetch('http://127.0.0.1:5000/api/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username: varUsername,
+                password: varPassword
+            })
         })
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        if (data.user_id) {
-            // Guarda como clave y valor en el localstorage
-            localStorage.setItem('user_id', data.user_id);
+        .then(res => res.json())
+        .then(data => {
+            loader.style.display = 'none'; // Oculta el loader
+
+            console.log(data);
+            if (data.user_id) {
+                // Guarda como clave y valor en el localstorage
+                localStorage.setItem('user_id', data.user_id);
+                M.toast({
+                    html: data.mensaje,
+                    classes: 'green'
+                });
+                location.href = 'index.html';
+            } else {
+                M.toast({
+                    html: data.error,
+                    classes: 'red'
+                });
+            }
+        })
+        .catch(err => {
+            loader.style.display = 'none'; // Oculta el loader
+
             M.toast({
-                html: data.mensaje,
-                classes: 'green'
-            });
-        } else {
-            M.toast({
-                html: data.error,
+                html: "Error de conexión",
                 classes: 'red'
             });
-        }
-    })
-    .catch(err => {
-        M.toast({
-            html: "Error de conexión",
-            classes: 'red'
         });
-    })
+    }, 2000);
 });
