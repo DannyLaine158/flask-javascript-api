92ab06d181d0c027f6efe6b3f557ae0541c0c26f
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2025-05-09 13:56:52 -0600

    Add files via upload

--- frontend/javascript/home.js
+++ frontend/javascript/home.js
@@ -1,21 +1,29 @@
-const userId = parseInt(localStorage.getItem("user_id"));
-if (!userId)
-    window.location.href = './login.html';
-
-fetch('http://127.0.0.1:5000/api/users')
-.then(res => res.json())
-.then(users => {
-    console.log(users);
+function loadData() {
+    const userId = parseInt(localStorage.getItem("user_id"));
+    if (!userId)
+        window.location.href = './login.html';
     
-    // Obtenemos el elemento html que vamos a rellenar
-    const greeting = document.getElementById("user-greeting");
-    // De todos los usuarios encontramos al que sea el propio
-    const user = users.find(u => u.id === userId);
-    // Llenamos el html con el usuario encontrado
-    greeting.textContent = `Bienvenido ${user.username}`;
-});
+    fetch('http://127.0.0.1:5000/api/users')
+    .then(res => res.json())
+    .then(users => {
+        console.log(users);
+        
+        // Obtenemos el elemento html que vamos a rellenar
+        const greeting = document.getElementById("user-greeting");
+        // De todos los usuarios encontramos al que sea el propio
+        const user = users.find(u => u.id === userId);
+        // Llenamos el html con el usuario encontrado
+        greeting.textContent = `Bienvenido ${user.username}`;
+    });
+}
 
-document.getElementById("btn-logout").addEventListener("click", () => {
-    localStorage.removeItem("user_id");
-    window.location.href = '/frontend/login.html';
-})
+document.addEventListener("DOMContentLoaded", () => {
+    const imageInput = document.getElementById("imageInput");
+    const fabUpload = document.getElementById("fab-upload");
+
+    fabUpload.addEventListener("click", () => {
+        imageInput.click();
+    });
+    
+    loadData();
+});
