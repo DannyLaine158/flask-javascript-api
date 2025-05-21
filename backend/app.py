07bcd8086edfec5735c245df36d46f84a2485e1b
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2025-05-20 19:14:01 -0600

    Upload config for deployment

--- backend/app.py
+++ backend/app.py
@@ -22,6 +22,8 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
+
+    # Modo Producción
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
